77ced6ee0735da5691107fb56fd56b67bec89422
milkygraph <50379548+milkygraph@users.noreply.github.com>
2023-04-04 14:08:23 +0300

    Add sr_flip_flop implementation

--- Lab_hw2/hw_2.srcs/sources_1/new/JK_flip_flop.v
+++ Lab_hw2/hw_2.srcs/sources_1/new/JK_flip_flop.v
@@ -34,3 +34,49 @@ module JK_flip_flop(input J, input K, input clk, output Q, output Qn);
     SR_flip_flop JK_flipflop(S, R, clk, Q, Qn);
        
 endmodule
+
+module sr_latch_with_enable(
+    input S,
+    input R,
+    input E,
+    output Q,
+    output Q_neg
+    );
+    wire Snot, Rnot;
+    
+    nand_gate nand1(S, E, Snot);
+    nand_gate nand2(R, E, Rnot);
+    nand_gate nand3(Snot, Q_neg, Q);
+    nand_gate nand4(Rnot, Q, Q_neg);    
+endmodule
+
+module sr_flip_flop(input D, input clk, output Q, output Qn);
+    wire NOT_clk;
+    wire NOT_D;
+    wire QM;
+    wire QMn;
+    
+    nand_gate NOT1(clk, clk, NOT_clk);
+    nand_gate NOT2(D, D, NOT_D);
+    
+    sr_latch_with_enable latch1(D, NOT_D, NOT_clk, QM, QMn);
+    sr_latch_with_enable latch2(QM, QMn, clk, Q, Qn);
+
+endmodule 
+
+module jk_flip_flop (input J, input K, input clk, output Q, output Qn);
+    wire J_and_Qn, J_and_Qn_not;
+    wire NOT_K;
+    wire K_and_Q, K_and_Q_not;
+    wire OR_out;
+    
+    nand_gate NOT1(K, K, NOT_K);
+    nand_gate AND2(NOT_K, Q, K_and_Q_not);
+    nand_gate NOT2(K_and_Q_not, K_and_Q_not, K_and_Q);
+    nand_gate AND3(J, Qn, J_and_Qn_not);
+    nand_gate NOT3(J_and_Qn_not, J_and_Qn_not, J_and_Qn);
+    nand_gate OR(K_and_Q_not, J_and_Qn_not, OR_out);
+    
+    sr_flip_flop flip(OR_out, clk, Q, Qn);
+
+endmodule
